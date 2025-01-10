a3f05a2c198747faea41310f701dcab91ccbc0ca
VIshal6620 <vishalvish7099@gmail.com>
2025-01-10 22:40:48 +0530

    10-01-2025

--- django-project-20/SOS/SOS/settings.py
+++ django-project-20/SOS/SOS/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vishalvish@gmail.com'
-EMAIL_HOST_PASSWORD = 'uhqn lrdu yeld wyrz'
+EMAIL_HOST_PASSWORD = '123'
